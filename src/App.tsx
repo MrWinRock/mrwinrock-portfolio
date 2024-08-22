450f4ce4936d3244233532bf1d56322416aaa451
MrWinRock <mrwinrock11@gmail.com>
2024-08-22 18:59:20 +0700

    setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import { Button } from "@material-tailwind/react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button
+          className=""
+          color="red"
+          placeholder={undefined}
+          onPointerEnterCapture={undefined}
+          onPointerLeaveCapture={undefined}
+        >
+          Click me!
+        </Button>
       </header>
     </div>
   );
